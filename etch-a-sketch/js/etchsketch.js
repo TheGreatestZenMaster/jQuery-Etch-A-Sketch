1ea29fc12eb8009e52080c3f7a87fa0bf9325a0a
Jake Loose <jetloose@gmail.com>
2016-05-20 09:33:44 +0900

    Add files via upload
    Changed from <div> to <table>.
    -Successfully removed border spacing and set the table to be 100% of the area available regardless of the number of cells.

--- etch-a-sketch/js/etchsketch.js
+++ etch-a-sketch/js/etchsketch.js
@@ -1,18 +1,18 @@
 var cellsOnASide = 80;
-var cell_height = "10px";
-var cell_width = "10px";
 
 $(document).ready(function(){
 	create_layout();
-	$('div.cell').mouseenter(function(){
+	$('td.cell').mouseenter(function(){
 		$(this).css("background", "black");
 	});
 });
 
 function create_layout(){
-	for(i=1; i <= (cellsOnASide*cellsOnASide); i++){
-		$('#wrapper').append('<div class="cell"></div>');
+	for(i=1; i <= cellsOnASide; i++){
+		$('table').append('<tr></tr>');
+		var currentRow = $('tr:last');
+		for(x=1; x <= cellsOnASide; x++){
+			currentRow.append('<td class="cell"></td>')
+		}
 	}
-	$('div.cell').css("height", cell_height);
-	$('div.cell').css("width", cell_width);
 }
